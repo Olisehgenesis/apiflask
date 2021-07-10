6d545d7adcbf559e6822c990602272e64ac0f3a8
Oliseh genesis <mwijusyaoliseh1@gmail.com>
2021-07-10 10:58:08 +0300

    changed api file

--- api.py
+++ api.py
@@ -1,11 +1,37 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+# Create a new Flask application
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+# Set up SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////data.db'
+db = SQLAlchemy(app)
 
+# Define a class for the Artist table
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Email = db.Column(db.String)
+    Password = db.Column(db.String)
+    School = db.Column(db.String)
+    Class = db.Column(db.String)
+    Type = db.Column(db.String)
+    Premium = db.Column(db.Boolean)
+
+# Create the table
+db.create_all()
+
+from marshmallow_jsonapi.flask import Schema
+from marshmallow_jsonapi import fields
+
+# Create data abstraction layer
+class UserSchema(Schema):
+    class Meta:
+        type_ = 'username'
+        self_view = 'artist_one'
+        self_view_kwargs = {'id': '<id>'}
+        self_view_many = 'artist_many'
+
+    id = fields.Integer()
+    name = fields.Str(required=True)
+    
